9693be70a3fca20958447b3c153c7a71b65fbfef
steven.yeo <steven.y@dynrtech.com>
2023-01-14 13:49:24 +0700

    Card Item Component

---
+++ src/contexts/product.context.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState, useEffect } from "react";
+
+import PRODUCTS from '../shop-data.json';
+
+export const ProductContext = createContext({
+    products: [],
+    setProducts: () => null
+});
+
+export const ProductProvider = ({ children }) => {
+    const [products, setProducts] = useState([]);
+    const value = { products, setProducts };
+
+    useEffect(() => {
+        setProducts(PRODUCTS);
+    }, []);
+
+    return (
+        <ProductContext.Provider value={value}>
+            { children }
+        </ProductContext.Provider>
+    )
+};
