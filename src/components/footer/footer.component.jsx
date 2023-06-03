446bc26e358d8aac3435c38a1cbcd5eaf897a265
steven.yeo <steven.y@dynrtech.com>
2023-06-03 19:11:35 +0700

    Implement Footer based on steby requirement

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,33 +1,54 @@
-import './footer.styles.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+import './footer.styles.css';
 
 var style = {
     backgroundColor: "#F8F8F8",
     borderTop: "1px solid #E7E7E7",
     textAlign: "center",
     padding: "20px",
-    position: "fixed",
+    marginTop: "auto",
     left: "0",
     bottom: "0",
-    height: "60px",
-    width: "100%",
+    width: "100%"
 }
 
 var phantom = {
   display: 'block',
   padding: '20px',
-  height: '60px',
+  height: '100px',
   width: '100%',
 }
 
 const Footer = () => {
     return (
-        <div>
-            <div style={phantom}>
-                <div style={style}>
-                    {
-                        `@kelompok 3`
-                    }
+        <div style={phantom}>
+            <div style={style} className='footerContainer'>
+                <div className={'flexContainer'}>
+                    <div className='flexChild'>
+                        <h3>Steby Cart</h3>
+
+                        <p>Kelompok-3 Develop shopping shop is from by steby group. the shop have shoes,shirt,and etc</p>
+                    </div>
+
+                    <div className='flexChild'>
+                        <h3>Made By</h3>
+                        <h5 style={{marginBottom: 0, marginTop: '5px'}}>HTML5</h5>
+                        <h5 style={{marginBottom: 0, marginTop: '5px'}}>CSS3</h5>
+                        <h5 style={{marginBottom: 0, marginTop: '5px'}}>JavaScript</h5>
+                        <h5 style={{marginBottom: 0, marginTop: '5px'}}>React JS</h5>
+                        <h5 style={{marginBottom: 0, marginTop: '5px'}}>Firebase</h5>
+                    </div>
+
+                    <div className='flexChild'>
+                        <h3>Contact</h3>
+                        <h5 style={{marginBottom: 0, marginTop: '5px'}}><FontAwesomeIcon icon={faPhone} /> +62 822-8741-3565</h5>
+                        <h5 style={{marginBottom: 0, marginTop: '5px'}}><FontAwesomeIcon icon={faEnvelope} /> steby.yeo@gmail.com</h5>
+                    </div>
                 </div>
+                <hr />
+                { '@ 2023 Copyright: Kelompok-3' }
             </div>
         </div>
     );
